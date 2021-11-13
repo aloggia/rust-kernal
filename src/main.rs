9d3f82e18584a4781b37b3f907560139b5f58ee8
aloggia <aloggia22@gmail.com>
2021-11-13 18:41:49 -0500

    Implemented println macro, to allow printing to console and display error messages

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::fmt::Write;
 
+mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -12,20 +14,14 @@ static HELLO: &[u8] = b"Hello World!";
 
 // Function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
 //defines the entry interface
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb
-        }
-    }
-    loop{}
+    println!("Hello World{}", "!");
+    loop {}
 }
