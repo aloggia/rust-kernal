ff3a62fff087373e12ad61d19d36af9ca50c06e2
aloggia <aloggia22@gmail.com>
2022-04-29 13:13:36 -0400

    I did a bunch of work a while ago but don't remember what I changed. Added a bunch of tests and the ability to communicate with serial  output

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,89 @@ use core::panic::PanicInfo;
 use core::fmt::Write;
 
 mod vga_buffer;
+mod serial;
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+
+impl<T> Testable for T
+where
+    T:Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]")
+    }
+}
+//QEMU exit interface
+#[derive(Debug, Clone, Copy, PartialE uartq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Succss = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 // Testing framework
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
+    exit_qemu(QemuExitCode::Succss);
 }
 #[test_case]
 fn trivial_assertion() {
-    print!("Trivial assertion");
     assert_eq!(1,1);
-    println!("[ok]");
+}
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+#[test_case]
+fn test_println_output() {
+    let s = "This is a single line test string for one line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
 
 // Function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop{}
+}
+
+// Panic handler for running test cases
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
 }
 
 //defines the entry interface
